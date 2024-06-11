db710d2d03076a04f43915b41e3897268f6b84fc
Riyas-menttech <Riyaz@ment.tech>
2024-06-11 11:07:09 +0530

    added mutation and  subscripton

--- server/src/Graphql/Schema.ts
+++ server/src/Graphql/Schema.ts
@@ -1,4 +1,10 @@
 import axios from "axios";
+import {PubSub} from 'graphql-subscriptions'
+
+const pubsub = new PubSub();
+const TODO_ADDED = 'TODO_ADDED';
+const TODO_UPDATED = 'TODO_UPDATED';
+const TODO_DELETED = 'TODO_DELETED';
 
 export const typeDefs = `
 type Geo {
@@ -32,6 +38,16 @@ getTodos:[Todo],
 getUsers:[User],
 getSingleUser(id:ID!):User
 }
+type Mutation {
+addTodo(userId: String!, title: String!, completed: Boolean):Todo
+deleteTodo(id:ID!):Todo
+updateTodo(id: ID!, userId: String, title: String, completed: Boolean): Todo
+}
+type Subscription {
+todoAdded:Todo
+todoUpdated:Todo
+todoDeleted:Todo
+}
 `;
 
 export const resolvers = {
@@ -54,4 +70,38 @@ export const resolvers = {
       ).data;
     },
   },
-};
+  Mutation :{
+    addTodo: async (_, { userId, title, completed }) => {
+        const newTodo = { userId, title, completed };
+        const response = await axios.post('https://jsonplaceholder.typicode.com/todos', newTodo);
+        const addedTodo = response.data;
+        pubsub.publish(TODO_ADDED,{todoAdded:addedTodo})
+        return addedTodo;
+  },
+  updateTodo: async (_, { id, userId, title, completed }) => {
+    const updatedFields = { userId, title, completed };
+    const response = await axios.put(`https://jsonplaceholder.typicode.com/todos/${id}`, updatedFields);
+    const updatedTodo = response.data;
+    pubsub.publish(TODO_UPDATED,{todoUpdated:updatedTodo});
+    return updatedTodo;
+  },
+  deleteTodo: async (_, { id }) => {
+    const response = await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
+    const deletedTodo = { id };
+    pubsub.publish(TODO_DELETED, { todoDeleted: deletedTodo });
+    return deletedTodo;
+}
+
+},
+Subscription : {
+    todoAdded:{
+        subscribe:()=>pubsub.asyncIterator([TODO_ADDED])
+    },
+    todoUpdated:{
+        subscribe:()=>pubsub.asyncIterator([TODO_UPDATED])
+    },
+    todoDeleted:{
+        subscribe:()=>pubsub.asyncIterator([TODO_DELETED])
+    }
+}
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,46 @@
 import { ApolloServer } from "@apollo/server";
-import {expressMiddleware} from '@apollo/server/express4'
-import express from 'express';
+import { expressMiddleware } from "@apollo/server/express4";
+import express from "express";
 import bodyParser from "body-parser";
-import cors from 'cors';
+import cors from "cors";
 import { Data } from "./Data.js";
-import { resolvers,typeDefs } from "./Graphql/Schema.js";
+import { subscribe, execute } from "graphql";
+import { SubscriptionServer } from "subscriptions-transport-ws";
+import { resolvers, typeDefs } from "./Graphql/Schema.js";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { createServer } from "http";
 
 async function startServer() {
-    const app = express()
-    const server = new ApolloServer({typeDefs,resolvers});  
+  const port = 8000;
+  const app = express();
+  const httpServer = createServer();
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
+  const server = new ApolloServer({ schema });
 
-    app.use(bodyParser.json());
-    app.use(cors());  
+  app.use(bodyParser.json());
+  app.use(cors());
 
-    await server.start();
-    
+  await server.start();
 
-    app.use('/graphql',expressMiddleware(server));
+  app.use("/graphql", expressMiddleware(server));
 
-    app.listen(8000,()=>console.log('Server started 8000'));
-} 
+  //Setup subscription websocket
+  SubscriptionServer.create(
+    {
+      execute,
+      subscribe,
+      schema,
+    },
+    {
+      server: httpServer,
+      path: "/graphql",
+    }
+  );
+  // Start the HTTP server
+  app.listen(8000, () => {
+    console.log("Server started on http://localhost:8000");
+    console.log("Subscriptions ready at ws://localhost:8000/graphql");
+  });
+}
 
-startServer()
+startServer();
